200881b977c0268b09ef8ebc45aa272a2e28e4fb
jules chaidron <jules.chaidron@insa-rennes.fr>
2025-11-04 16:43:17 +0100

    Updated generator to succesfully display in .txt file and in terminal initial state of the game (first draft)

--- FourchLang/packages/cli/src/generator.ts
+++ FourchLang/packages/cli/src/generator.ts
@@ -2,18 +2,54 @@ import type { Model } from 'fourch-lang-language';
 import { expandToNode, toString } from 'langium/generate';
 import * as fs from 'node:fs';
 import { extractDestinationAndName } from './util.js';
+import ansiStyles from 'ansi-styles';
 
 export function generateOutput(model: Model, source: string, destination: string): string {
     const data = extractDestinationAndName(destination);
 
+    let enemies : string[] = model.enemies.map(e => 
+        "("+e.x+","+e.y+")");
+    let enemyBodies : string[] = model.enemyBodies.map(e => 
+        "("+e.x+","+e.y+")");
+    let snakeBodyParts : string[] = model.snakeBodies.map(s => 
+        "("+s.x+","+s.y+")");
+    let fruits : string[] = model.fruits.map(f => 
+        "("+f.x+","+f.y+")");
+    let walls : string[] = model.walls.map(w => 
+        "("+w.x+","+w.y+")");
+
+    let grid = "";
+    let consoleGrid = "";
+    for (let s = 0; s < +model.grid.map(g => g.x)[0] + 2; s++) { grid += "# "; consoleGrid += `${ansiStyles.white.open}# ${ansiStyles.white.close}`; }
+    grid += "\n"; consoleGrid += "\n";
+    for (let i = 0; i < +model.grid.map(g => g.y)[0]; i++) {
+        grid += "# "; consoleGrid += `${ansiStyles.white.open}# ${ansiStyles.white.close}`;
+        for (let j = 0; j < +model.grid.map(g => g.x)[0]; j++) {
+            if (+model.player.map(p => p.x)[0] == j && +model.player.map(p => p.y)[0] == i) {
+                grid += "A "; consoleGrid += `${ansiStyles.green.open}A ${ansiStyles.green.close}`;
+            } else if (snakeBodyParts.includes("("+j+","+i+")")) {
+                grid += "S "; consoleGrid += `${ansiStyles.green.open}S ${ansiStyles.green.close}`;
+            } else if (enemies.includes("("+j+","+i+")")) {
+                grid += "M "; consoleGrid += `${ansiStyles.red.open}M ${ansiStyles.red.close}`;
+            } else if (enemyBodies.includes("("+j+","+i+")")) {
+                grid += "X "; consoleGrid += `${ansiStyles.red.open}X ${ansiStyles.red.close}`;
+            } else if (fruits.includes("("+j+","+i+")")) {
+                grid += "F "; consoleGrid += `${ansiStyles.yellow.open}F ${ansiStyles.yellow.close}`;
+            } else if (walls.includes("("+j+","+i+")")) {
+                grid += "# "; consoleGrid += `${ansiStyles.white.open}# ${ansiStyles.white.close}`;
+            } else {
+                grid += ". "; consoleGrid += `${ansiStyles.blue.open}. ${ansiStyles.blue.close}`;
+            }
+        }
+        grid += "#\n"; consoleGrid += `${ansiStyles.white.open}#${ansiStyles.white.close}\n`;
+    }
+    for (let s = 0; s < +model.grid.map(g => g.x)[0] + 2; s++) { grid += "# "; consoleGrid += `${ansiStyles.white.open}# ${ansiStyles.white.close}`; }
+
     // Edition
     const fileNode = expandToNode`
-
-        Testing ${source} generation.
-        player name is ${model.player.map(player => player.name).join(', ')}
-        grid size is ${model.grid.map(g => g.x)} x ${model.grid.map(g => g.y)}
-        add grid below : 
+        ${grid}
     `.appendNewLineIfNotEmpty();
+    console.log(consoleGrid);
     if (!fs.existsSync(data.destination)) {
         fs.mkdirSync(data.destination, { recursive: true });
     }
