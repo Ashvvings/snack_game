db3d0374356b8a42896a1852fd4dbfc538c95b89
Jade Vaillant <jade.vaillant@insa-rennes.fr>
2025-11-19 17:33:57 +0100

    validator

---
+++ FourchLang/fourch-lang/packages/language/test/parsing.test.ts
@@ -0,0 +1,20 @@
+import { describe } from "vitest";
+
+/*
+let services: ReturnType<typeof createFourchLangServices>;
+let parse:    ReturnType<typeof parseHelper<Model>>;
+let document: LangiumDocument<Model> | undefined;
+
+beforeAll(async () => {
+    services = createFourchLangServices(EmptyFileSystem);
+    parse = parseHelper<Model>(services.FourchLang);
+
+    // activate the following if your linking test requires elements from a built-in library, for example
+    // await services.shared.workspace.WorkspaceManager.initializeWorkspace([]);
+});
+*/
+
+describe('Parsing tests', () => {
+
+    // TODO: Add parsing tests
+});
